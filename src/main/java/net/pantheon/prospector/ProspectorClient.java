20a4214b492fc6882036bcfef894552139196b75
Mendaxx <iriseye.marblemellow@gmail.com>
2026-03-20 21:31:57 -0400

    Initial restructuring

--- src/main/java/net/pantheon/prospector/ProspectorClient.java
+++ src/main/java/net/pantheon/prospector/ProspectorClient.java
@@ -1,4 +1,4 @@
-package com.pantheon.prospector;
+package net.pantheon.prospector;
 
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
@@ -11,9 +11,9 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = Prospector.MODID, dist = Dist.CLIENT)
+@Mod(value = Prospector.MOD_ID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = Prospector.MODID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = Prospector.MOD_ID, value = Dist.CLIENT)
 public class ProspectorClient {
     public ProspectorClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
